e9cd4fa2bd56767aeb44a58477b66e167d9bf35c
Liyang Wang <wangliyang9933@gmail.com>
2025-03-13 13:23:02 +1000

    updated

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
+  // baseURL: 'http://localhost:5001', // local
   baseURL: 'http://13.238.143.99:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
